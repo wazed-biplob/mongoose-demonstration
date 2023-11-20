c6e2b281fae85fa00f9d4833375c33ab7e7f9101
= <www.biplob@gmail.com>
2023-11-20 23:47:34 +0600

    Validation Using Mongoose

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -7,7 +7,7 @@ import {
 } from './students.interface';
 
 const usernameSchema = new Schema<UserName>({
-  fName: { type: String, required: true },
+  fName: { type: String, required: [true, 'First Name is required'] },
   middleName: { type: String },
   lname: { type: String, required: true },
 });
@@ -28,20 +28,35 @@ const localGuardianSchema = new Schema<LocalGuardian>({
   address: { type: String },
 });
 const studentSchema = new Schema<Student>({
-  id: { type: Number, required: true },
-  name: usernameSchema,
-  gender: ['male', 'female'],
+  id: { type: Number, required: true, unique: true },
+  name: { type: usernameSchema, required: true },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not valid',
+    },
+    required: true,
+  },
   dataOfBirth: { type: String },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   contactNumber: { type: String, required: true },
   emergencyContact: { type: String, required: true },
-  bloodgroup: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-'],
+  bloodgroup: {
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-'],
+    },
+  },
   presentAddress: { type: String },
   permanentAddress: { type: String },
-  guardian: guadianSchema,
-  localGuardian: localGuardianSchema,
+  guardian: { type: guadianSchema, required: true },
+  localGuardian: { type: localGuardianSchema, required: true },
   profileImage: { type: String },
-  isActive: ['active', 'blocked'],
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 });
 
 export const StudentModel = model<Student>('student', studentSchema);
