5c8cb84bdc4fa3c04c4203ebd1b6dd1479ff9c49
= <www.biplob@gmail.com>
2023-11-21 21:49:08 +0600

    TS interface + Zod + Mongoose Bugs Fixed

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -26,13 +26,13 @@ const createStudent = async (req: Request, res: Response) => {
     // }
     res.status(200).json({
       success: true,
-      message: 'Student has been created successfully',
+      message: 'Student has been created successfully.',
       data: result,
     });
   } catch (err) {
     res.status(400).json({
       success: false,
-      message: 'Something went wrong!',
+      message: 'Something went wrong',
       error: `Error Message : ` + err,
     });
   }
